1f6a869e4258d09c4dfb6825f5c13696eac417a8
priyanshu kumar <58341603+priyanshu294@users.noreply.github.com>
2020-07-08 16:20:33 +0530

    Update ExampleUnitTest.java
    Changed...

--- app/src/test/java/com/journaldev/recyclerviewmultipleviewtype/ExampleUnitTest.java
+++ app/src/test/java/com/journaldev/recyclerviewmultipleviewtype/ExampleUnitTest.java
@@ -1,4 +1,6 @@
 package com.journaldev.recyclerviewmultipleviewtype;
+import android.content.Context;
+import androidx.test.core.app.ApplicationProvider;
 
 import org.junit.Test;
 
@@ -7,9 +9,62 @@ import static org.junit.Assert.*;
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
+// Local unit test
 public class ExampleUnitTest {
+   
+    private static final String FAKE_STRING = "HELLO_WORLD";
+    private Context context = ApplicationProvider.getApplicationContext();
+
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void readStringFromContext_LocalizedString() {
+        // Given a Context object retrieved from Robolectric...
+        ClassUnderTest myObjectUnderTest = new ClassUnderTest(context);
+
+        // ...when the string is returned from the object under test...
+        String result = myObjectUnderTest.getHelloWorldString();
+
+        // ...then the result should be the expected one.
+        assertThat(result).isEqualTo(FAKE_STRING);
+    }
+}
+
+// UI test
+
+package com.example.android.testing.espresso.BasicSample;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class ChangeTextBehaviorTest {
+
+    private String stringToBetyped;
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule
+            = new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void initValidString() {
+        // Specify a valid string.
+        stringToBetyped = "Espresso";
+    }
+
+    @Test
+    public void changeText_sameActivity() {
+        // Type text and then press the button.
+        onView(withId(R.id.editTextUserInput))
+                .perform(typeText(stringToBetyped), closeSoftKeyboard());
+        onView(withId(R.id.changeTextBt)).perform(click());
+
+        // Check that the text was changed.
+        onView(withId(R.id.textToBeChanged))
+                .check(matches(withText(stringToBetyped)));
     }
 }
